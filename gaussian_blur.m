cf8d509a3b612017c93a4ed736017a9fd108fedc
chou <58171830+chou17@users.noreply.github.com>
2022-03-21 19:55:15 +0800

    Rename hw2.m to gaussian_blur.m

--- gaussian_blur.m
+++ gaussian_blur.m
@@ -1,9 +1,9 @@
 clear,clc;
 
-% Åª¨ú¹Ï¹³
-Img = imread('®ã¤ü­¸­¸.jpg');
-imwrite(Img,'./result/®ã¤ü­¸­¸.jpg');
-%ÂX¥R3*3
+% Ã…ÂªÂ¨ÃºÂ¹ÃÂ¹Â³
+Img = imread('Â®Ã£Â¤Ã¼Â­Â¸Â­Â¸.jpg');
+imwrite(Img,'./result/Â®Ã£Â¤Ã¼Â­Â¸Â­Â¸.jpg');
+%Ã‚XÂ¥R3*3
 img = double(Img);
 img_add = img(2,:,:);
 img_arr = [img_add;img];
@@ -17,7 +17,7 @@ img_add = [img(1,640,:);img(:,639,:)];
 img_add = [img_add;img(640,640,:)];
 img_arr = [img_arr img_add];
 
-% ³Ğ«ØÂoªi¾¹
+% Â³ÃÂ«Ã˜Ã‚oÂªiÂ¾Â¹
 W = fspecial('gaussian',[3,3]); 
 
 for i=1:3
@@ -33,7 +33,7 @@ imwrite(uint8(img),'./result/a_1.jpg');
 [peaksnr, snr] = psnr(uint8(img), Img);
 fprintf('\n The Peak-SNR value is %0.4f', peaksnr);
 
-%ÂX¥R7*7
+%Ã‚XÂ¥R7*7
 img_add = img(2:4,:,:);
 img_arr = [img_add;img];
 img_add = img(637:639,:,:);
@@ -46,7 +46,7 @@ img_add = [img(1:3,638:640,:);img(:,637:639,:)];
 img_add = [img_add;img(638:640,638:640,:)];
 img_arr = [img_arr img_add];
 
-% ³Ğ«ØÂoªi¾¹
+% Â³ÃÂ«Ã˜Ã‚oÂªiÂ¾Â¹
 W = fspecial('gaussian',[7,7]); 
 
 for i=1:3
@@ -64,7 +64,7 @@ fprintf('\n The Peak-SNR value is %0.4f', peaksnr);
 
 
 
-%ÂX¥R13*13
+%Ã‚XÂ¥R13*13
 img_add = img(2:7,:,:);
 img_arr = [img_add;img];
 img_add = img(634:639,:,:);
@@ -77,7 +77,7 @@ img_add = [img(1:6,635:640,:);img(:,634:639,:)];
 img_add = [img_add;img(635:640,635:640,:)];
 img_arr = [img_arr img_add];
 
-% ³Ğ«ØÂoªi¾¹
+% Â³ÃÂ«Ã˜Ã‚oÂªiÂ¾Â¹
 W = fspecial('gaussian',[13,13]); 
 
 for i=1:3
@@ -93,11 +93,11 @@ imwrite(uint8(img),'./result/a_3.jpg');
 [peaksnr, snr] = psnr(uint8(img), Img);
 fprintf('\n The Peak-SNR value is %0.4f', peaksnr);
 
-%bÂX¥R3*3
+%bÃ‚XÂ¥R3*3
 img_arr = img_arr(6:647,6:647,:);
 
 
-% ³Ğ«ØÂoªi¾¹
+% Â³ÃÂ«Ã˜Ã‚oÂªiÂ¾Â¹
 W = fspecial('gaussian',[3,3],1); 
 
 for i=1:3
@@ -113,7 +113,7 @@ imwrite(uint8(img),'./result/b_1.jpg');
 [peaksnr, snr] = psnr(uint8(img), Img);
 fprintf('\n The Peak-SNR value is %0.4f', peaksnr);
 
-% ³Ğ«ØÂoªi¾¹
+% Â³ÃÂ«Ã˜Ã‚oÂªiÂ¾Â¹
 W = fspecial('gaussian',[3,3],30); 
 
 for i=1:3
@@ -129,7 +129,7 @@ imwrite(uint8(img),'./result/b_2.jpg');
 [peaksnr, snr] = psnr(uint8(img), Img);
 fprintf('\n The Peak-SNR value is %0.4f', peaksnr);
 
-% ³Ğ«ØÂoªi¾¹
+% Â³ÃÂ«Ã˜Ã‚oÂªiÂ¾Â¹
 W = fspecial('gaussian',[3,3],100); 
 
 for i=1:3
